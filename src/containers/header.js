e595719ccc5a7582837eebc475046bacf4ebab8d
Jeffrey <6518347+jeffreytu@users.noreply.github.com>
2020-09-20 23:28:54 -0700

    add feature component, add header styled component

--- src/containers/header.js
+++ src/containers/header.js
@@ -1,13 +1,16 @@
 import React from "react";
-import Header from "../components";
+import { Header } from "../components";
+import * as ROUTES from "../constants/routes";
+import logo from "../logo.svg";
 
-export function HeaderContainer() {
+export function HeaderContainer({ children }) {
   return (
     <Header>
       <Header.Frame>
-        <Header.Logo />
-        <Header.Button />
+        <Header.Logo to={ROUTES.HOME} alt="Netflix" src={logo} />
+        <Header.ButtonLink to={ROUTES.SIGN_IN}>Sign In</Header.ButtonLink>
       </Header.Frame>
+      {children}
     </Header>
   );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { HeaderContainer } from "../containers/header";
+import { Feature, OptForm } from "../components";
 import { JumbotronContainer } from "../containers/jumbotron";
 import { FooterContainer } from "../containers/footer";
 import { FaqsContainer } from "../containers/faqs";
@@ -8,10 +9,25 @@ export default function Home() {
   return (
     <React.Fragment>
       <HeaderContainer>
-        <JumbotronContainer />
-        <FaqsContainer />
-        <FooterContainer />
+        <Feature>
+          <Feature.Title>Unlimited films, TV shows and more.</Feature.Title>
+          <Feature.Subtitle>
+            Watch anywhere. Cancel at any time.
+          </Feature.Subtitle>
+          <OptForm>
+            <OptForm.Input placeholder="E-mail Address" />
+            <OptForm.Button>Try it now</OptForm.Button>
+            <OptForm.Break />
+            <OptForm.Text>
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </OptForm.Text>
+          </OptForm>
+        </Feature>
       </HeaderContainer>
+      <JumbotronContainer />
+      <FaqsContainer />
+      <FooterContainer />
     </React.Fragment>
   );
 }
