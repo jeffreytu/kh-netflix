f2eb13de3ea0b32cb057b30ae0635a2b2757ac42
Jeffrey Tu <6518347+jeffreytu@users.noreply.github.com>
2020-09-20 22:14:47 -0700

    add header component

---
+++ src/containers/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components";
+
+export function HeaderContainer() {
+  return (
+    <Header>
+      <Header.Frame>
+        <Header.Logo />
+        <Header.Button />
+      </Header.Frame>
+    </Header>
+  );
+}
