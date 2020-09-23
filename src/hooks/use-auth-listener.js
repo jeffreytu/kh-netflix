5e9d2fea1ab9152196c2a2f29dbd1b042bc25a74
Jeffrey <6518347+jeffreytu@users.noreply.github.com>
2020-09-22 23:04:13 -0700

    useContent hook for browse page

--- src/hooks/use-auth-listener.js
+++ src/hooks/use-auth-listener.js
@@ -5,7 +5,6 @@ export default function useAuthListener() {
   const [user, setUser] = useState(
     JSON.parse(localStorage.getItem("authUser"))
   );
-
   const { firebase } = useContext(FirebaseContext);
 
   useEffect(() => {
@@ -20,7 +19,7 @@ export default function useAuthListener() {
     });
 
     return () => listener();
-  });
+  }, [firebase]);
 
   return { user };
 }
