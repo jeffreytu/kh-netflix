e54905dab629ab7193842af981828a2fd70fcee7
Jeffrey <6518347+jeffreytu@users.noreply.github.com>
2020-09-23 23:30:47 -0700

    add profiles component

---
+++ src/containers/browse.js
@@ -0,0 +1,31 @@
+import React, { useState, useContext, useEffect } from "react";
+import { SelectProfileContainer } from "./profiles";
+import { FirebaseContext } from "../context/firebase";
+import { Loading, Header } from "../components";
+
+export function BrowseContainer({ slides }) {
+  const [profile, setProfile] = useState({});
+  const [loading, setLoading] = useState(true);
+  const { firebase } = useContext(FirebaseContext);
+  const user = firebase.auth().currentUser || {};
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, [profile.displayName]);
+
+  return profile.displayName ? (
+    <React.Fragment>
+      {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
+      <Header src="joker1">
+        <p>Hello</p>
+      </Header>
+    </React.Fragment>
+  ) : (
+    <SelectProfileContainer
+      user={user}
+      setProfile={setProfile}
+    ></SelectProfileContainer>
+  );
+}
